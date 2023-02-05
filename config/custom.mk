2bfe0796afa7dc8a65db7bdf4189b17ea64e109c
EndCredits <alicization.han@gmail.com>
2023-02-05 23:23:25 +0800

    packages: Import prebuilt Camera Go
     * From https://www.celsoazevedo.com/files/android/google-camera/dev-shamim/f/dl14/
    
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- config/custom.mk
+++ config/custom.mk
@@ -11,3 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Vendor Path
+CUSTOM_PATH := vendor/hana
+
+# Soong Namespace
+PRODUCT_SOONG_NAMESPACES += $(CUSTOM_PATH)
+
+# Custom Apps
+PRODUCT_PACKAGES += \
+	CameraGo_SHAMIM
