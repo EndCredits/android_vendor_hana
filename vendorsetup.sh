991c779d169520d6539925c14df352607b3ed227
EndCredits <alicization.han@gmail.com>
2023-02-22 23:29:00 +0800

    packages: Move kprofiles into source tree
    Signed-off-by: EndCredits <alicization.han@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,23 @@
+# Copyright 2023 Crepusuclar Hana
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ ! -d "$ANDROID_BUILD_TOP/packages/apps/KProfiles" ]; then
+    echo 'KProfiles App not exist. Cloning...'
+    git clone https://github.com/CannedShroud/android_packages_apps_KProfiles $ANDROID_BUILD_TOP/packages/apps/KProfiles/
+else
+    echo 'KProfiles App seems existed. Updating to latest version'
+    cd $ANDROID_BUILD_TOP/packages/apps/KProfiles
+    git pull
+    croot
+fi
