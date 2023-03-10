460cd3f966baedf0e4a59d15df82004c89fce518
EndCredits <alicization.han@gmail.com>
2023-03-10 23:11:17 +0800

    tools: AppUpdater: Add a simple script to update apps
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- tools/get_app_updates.sh
+++ tools/get_app_updates.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
+#
 # Copyright 2023 Crepusuclar Hana
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# SPDX-License-Identifier: MIT
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 CURR_DIR=$(pwd)
 
 PACKAGES_PATH=$CURR_DIR/packages
 
-wget -O $PACKAGES_PATH/AndroidDataBackup/DataBackup.apk $(curl -s https://api.github.com/repos/XayahSuSuSu/Android-DataBackup/releases/latest | jq -r ".assets[].browser_download_url")
+wget -O $PACKAGES_PATH/AndroidDataBackup/DataBackup.apk $(curl -s https://api.github.com/repos/XayahSuSuSu/Android-DataBackup/releases/latest | jq -r ".assets[].browser_download_url" | grep arm64 | grep foss )
 wget -O $PACKAGES_PATH/ViaBrowser/via.apk https://res.viayoo.com/v1/via-release.apk
-wget -O $PACKAGES_PATH/Fcitx5Android/fcitx5android.apk $(curl -s https://api.github.com/repos/fcitx5-android/fcitx5-android/releases/latest | jq -r ".assets[].browser_download_url" | grep arm64)
+wget -O $PACKAGES_PATH/Fcitx5Android/fcitx5android.apk $(curl -s https://api.github.com/repos/fcitx5-android/fcitx5-android/releases/latest | jq -r ".assets[].browser_download_url" | grep arm64 )
